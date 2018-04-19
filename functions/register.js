fcda860eea07f274dbb4d69b4708e07b57eb8998
tbachosz <t.bachosz@gmail.com>
2018-04-19 23:55:49 +0400

    Fix3

--- functions/register.js
+++ functions/register.js
@@ -16,7 +16,7 @@ exports.registerUser = req =>
 		const newUser = new user({
 			name: req.body.name,
 
-			private_key =  randomstring.generate(12),
+			private_key:randomstring.generate(12),
 			created_at: new Date(),
 			refresh_token: refreshToken,
 
