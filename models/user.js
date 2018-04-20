dcfe7cdd9a0501f195aebcf77a2d9c8d7543abea
wl1390 <wl1390@nyu.edu>
2018-04-20 19:27:04 +0400

    test

--- models/user.js
+++ models/user.js
@@ -22,5 +22,5 @@ const userSchema = mongoose.Schema({
 });
 
 mongoose.Promise = global.Promise;
-mongoose.createConnection(config.db);
-module.exports = mongoose.model('user', userSchema);
+var connection = mongoose.createConnection(config.db);
+module.exports = connection.model('user', userSchema);
