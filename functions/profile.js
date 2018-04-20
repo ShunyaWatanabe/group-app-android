947b2bf57d976b720660775c17af32aac08390e4
wl1390 <wl1390@nyu.edu>
2018-04-20 22:39:11 +0400

    test

--- functions/profile.js
+++ functions/profile.js
@@ -8,6 +8,7 @@ exports.getProfile = privateKey =>
 	new Promise((resolve,reject) => {
 		user.find({ private_key: privateKey })
 			.then(users => {
+				console.log("octobertest1");
 				console.log("PRIVATE KEY 3",privateKey);
 			if (users.length == 0) {
 				console.log("PRIVATE KEY 4",privateKey);
