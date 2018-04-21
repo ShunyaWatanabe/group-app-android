2f3db9d38e7a03d2d9a877e56d1c826ac90d426c
wl1390 <wl1390@nyu.edu>
2018-04-21 23:17:33 +0400

    test

--- functions/getGroup.js
+++ functions/getGroup.js
@@ -5,11 +5,15 @@ const group = require('../models/group');
 
 exports.getGroup = (req) =>
 	new Promise((resolve,reject) => {
-		console.log("test1");
 
 		user.findOne({'private_key':req.params.private_key},function(err,doc){
 			console.log("test00");
 			if (err) console.log(err);
-			resolve({status:201,message:"sucessfully get group", groups:doc.groups_participated });
+			var temp = doc.groups_participated;
+			if (temp!=null){
+				resolve({status:201,message:"sucessfully get group", groups:doc.groups_participated });
+			}else{
+				resolve({status:201,message:"sucessfully get group",groups:null});
+			}
 		});		
 	});
