68b442f7cd6516ed9520eeb7fe5d5fed4de01b40
wl1390 <wl1390@nyu.edu>
2018-04-21 18:57:06 +0400

    test

--- functions/changeUserName.js
+++ functions/changeUserName.js
@@ -4,23 +4,26 @@ const user = require('../models/user');
 const group = require('../models/group');
 
 
-// function changeUserName = (req) => 
-// 		new Promise((resolve,reject)=>{
-		
-// 			user.find({'private_key':req.body[1]},function(err,doc){
-// 					console.log('test2');
-// 				if (err) console.log(err);
-// 					console.log('test3');
-// 				doc.name = req.body[2];
-// 					console.log('test4');
-// 				doc.save();
-// 					console.log('test5');
-
-// 			});		
-// 		});
 	
 
 exports.changeUserName = (req) =>
 	new Promise((resolve,reject) => {
-		console.log(req.body);	
+		console.log(req.body);
+
+
+
+		user.find({'private_key':req.body[1]},function(err,doc){
+					console.log('test2');
+				if (err) console.log(err);
+					console.log('test3');
+				doc.name = req.body[2];
+					console.log('test4');
+				doc.save();
+					console.log('test5');
+
+			});		
+
+
+
+
 	});
