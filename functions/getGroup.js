de43f4717fbdb45d80978af9f9ce69ec875ea400
wl1390 <wl1390@nyu.edu>
2018-04-21 23:23:55 +0400

    test

--- functions/getGroup.js
+++ functions/getGroup.js
@@ -9,11 +9,10 @@ exports.getGroup = (req) =>
 		user.findOne({'private_key':req.params.private_key},function(err,doc){
 			console.log("test00");
 			if (err) console.log(err);
-			var temp = doc.groups_participated;
-			if (temp!=null){
-				resolve({status:201,message:"sucessfully get group", groups:doc.groups_participated });
-			}else{
-				resolve({status:201,message:"sucessfully get group",groups:null});
-			}
+			
+			resolve({status:201,message:"sucessfully get group", groups:doc.groups_participated })
+			.then(console.log("ssss"))
+			.catch(console.log('ffff'));
+
 		});		
 	});
