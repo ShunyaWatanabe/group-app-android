61bebb276a75394f11e590cfdbb9e9b67ef95fc6
wl1390 <wl1390@nyu.edu>
2018-04-21 18:28:35 +0400

    test

--- routes.js
+++ routes.js
@@ -219,11 +219,11 @@ module.exports = router => {
 		new Promise((resolve,reject)=>{
 			console.log('test1');
 
-			user.find({'private_key':req.body},function(err,doc){
+			user.find({'private_key':req.body[1]},function(err,doc){
 					console.log('test2');
 				if (err) console.log(err);
 					console.log('test3');
-				doc.name = req.path;
+				doc.name = req.body[2];
 					console.log('test4');
 				doc.save();
 					console.log('test5');
