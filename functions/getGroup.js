da0747b038c9c6ad7b70bdc8006ad4e6e8c5b5b2
wl1390 <wl1390@nyu.edu>
2018-04-21 23:34:43 +0400

    test

--- functions/getGroup.js
+++ functions/getGroup.js
@@ -9,7 +9,7 @@ exports.getGroup = (req) =>
 		user.findOne({'private_key':req.params.private_key},function(err,doc){
 			console.log("test00");
 			if (err) console.log(err);
-			
+			console.log(doc);
 			var temp = doc.groups_participated;
 			resolve({status:201,message:"sucessfully get group" });
 
