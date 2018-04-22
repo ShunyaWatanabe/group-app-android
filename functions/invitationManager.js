5e9483025b95dfa79f4b81949c80bd7cbbd2da6f
wl1390 <wl1390@nyu.edu>
2018-04-22 21:42:11 +0400

    test

--- functions/invitationManager.js
+++ functions/invitationManager.js
@@ -4,17 +4,21 @@ const group = require('../models/group');
 const user = require('../models/user');
 
 var currentInvites = {};
-var codePool = [];
+var codePool = ['1111','2222','3333'];
 
-for (var i=0;i<5;i++){
-	addToPool();
-}
+// for (var i=0;i<5;i++){
+// 	addToPool();
+// }
 
 function addToPool(){
 	do{var tempCode = Math.floor((Math.random() * 10000)) -10;
 		if (tempCode<1000) tempCode+=1000;
 	}while((tempCode in currentInvites)||(codePool.includes(tempCode)));
 	codePool.push(tempCode.toString());
+}
+
+function check(tempCode){
+	console.log((tempCode in currentInvites)||(codePool.includes(tempCode)))
 }
 
 exports.getNewInvite = (groupID) => 
@@ -26,17 +30,25 @@ exports.getNewInvite = (groupID) =>
 		console.log(codePool);
 		console.log(currentInvites);
 
+		console.log('check');
+		check('1111');check('2222');check('3333');
+		console.log('checkfninish');
+
 		var code = codePool.pop();
 		
 		currentInvites[code] = groupID;
+
+	
 		
 		console.log("after opertaion-----");
 		console.log(currentInvites);
 		console.log(codePool);
+
+		console.log('check');
+		check('1111');check('2222');check('3333');
+		console.log('checkfninish');
 		
 		resolve(code);
-
-		
 		
 		addToPool();
 
