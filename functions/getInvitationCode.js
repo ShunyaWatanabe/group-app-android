2833145c2f2e8b06bc044944030d4030c525e6fe
wl1390 <wl1390@nyu.edu>
2018-04-22 12:49:57 +0400

    test group create

--- functions/getInvitationCode.js
+++ functions/getInvitationCode.js
@@ -5,7 +5,12 @@
 exports.getInvitationCode = (req) =>
 	new Promise((resolve,reject) => {
 
-		var tempCode = Math.floor((Math.random() * 10000) );
+		//get a code from 1000 to 9999
+		var tempCode = Math.floor((Math.random() * 10000)) -10;
+		if (tempCode<1000) tempCode+=1000;
+
+
+		//two bugs, first it is very slow
 
 
 		//change the format of group_id:	req.params.getinvitationcode
