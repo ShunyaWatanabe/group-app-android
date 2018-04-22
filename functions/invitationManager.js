80bc9f87e8201302c860bebcdc5605bcbcc2f2ab
wl1390 <wl1390@nyu.edu>
2018-04-22 21:37:00 +0400

    test

--- functions/invitationManager.js
+++ functions/invitationManager.js
@@ -6,7 +6,7 @@ const user = require('../models/user');
 var currentInvites = {};
 var codePool = [];
 
-for (var i=0;i<50;i++){
+for (var i=0;i<5,i++){
 	addToPool();
 }
 
@@ -36,12 +36,12 @@ exports.getNewInvite = (groupID) =>
 		
 		resolve(code);
 
-		console.log("add one------");
+		
 		
 		addToPool();
-	
-		console.log(codePool);
 
+		console.log("add one------");
+		console.log(codePool);
 		console.log("finish----------");
 		
 		// setTimeout(function(){ 
