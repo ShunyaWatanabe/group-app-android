79ecabe7c955625fc47b32490f9c9d740f096032
wl1390 <wl1390@nyu.edu>
2018-04-22 21:22:59 +0400

    test

--- functions/invitationManager.js
+++ functions/invitationManager.js
@@ -6,28 +6,25 @@ const user = require('../models/user');
 var currentInvites = {};
 var codePool = [1111,2222,3333];
 
-// for (var i=0;i<50;i++){
-// 	addToPool();
-// }
-console.log('test11');
+for (var i=0;i<50;i++){
+	addToPool();
+}
 
-// function addToPool(){
-// 	do{var tempCode = Math.floor((Math.random() * 10000)) -10;
-// 		if (tempCode<1000) tempCode+=1000;
-// 	}while((tempCode in currentInvites)||(codePool.includes(tempCode)));
-// 	codePool.push(tempCode);
-// }
+function addToPool(){
+	do{var tempCode = Math.floor((Math.random() * 10000)) -10;
+		if (tempCode<1000) tempCode+=1000;
+	}while((tempCode in currentInvites)||(codePool.includes(tempCode)));
+	codePool.push(tempCode);
+}
 
 exports.getNewInvite = (groupID) => {
-	console.log('test1');
-
+	
 	new Promise((resolve,reject) => {
-		console.log('test2');
-
+		
 		var code = codePool.pop();
 		console.log('test3');
 		currentInvites.code = groupID;
-		console.log('test4');
+		console.log(currentInvites);
 		console.log(code);
 		console.log("test222");
 		resolve(code);
