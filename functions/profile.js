61b6662ec1bccb7fbfe631f0e02219ec19c110b8
wl1390 <wl1390@nyu.edu>
2018-04-25 22:16:37 +0400

    test

--- functions/profile.js
+++ functions/profile.js
@@ -7,14 +7,11 @@ exports.getProfile = privateKey =>
 	new Promise((resolve,reject) => {
 		user.find({ private_key: privateKey })
 			.then(users => {
-				console.log("PRIVATE KEY 3",privateKey);
 			if (users.length == 0) {
-				console.log("PRIVATE KEY 4",privateKey);
 				reject({ status: 404, message: 'User Not Found !' });
 			} else {
 				resolve(users[0]);
 			}
-
 		})
 
 		.catch(err => reject({ status: 500, message: 'Internal Server Error !' }))
