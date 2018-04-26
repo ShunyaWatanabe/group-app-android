2d95bba3259bb958724d5a4cf4819319160f5715
wl1390 <wl1390@nyu.edu>
2018-04-26 12:53:43 +0400

    test

--- routes.js
+++ routes.js
@@ -269,7 +269,7 @@ module.exports = router => {
 				function(groupID,callback){
 					group.findById(groupID, "_id name",function(err,groupObject){
 						if (err) console.log(err);
-						groupslist.put(groupObject);
+						groupslist.add(groupObject);
 					})
 					.then(()=>callback(null));
 				},
